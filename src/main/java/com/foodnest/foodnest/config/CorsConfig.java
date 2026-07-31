879bb48813ad0d4de3d11c4a38ec53dd2a3f6b0f
Pradnya Kambale <kambalepradnya11@gmail.com>
2026-07-31 02:21:32 -0700

    FIXED CORS

--- src/main/java/com/foodnest/foodnest/config/CorsConfig.java
+++ src/main/java/com/foodnest/foodnest/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("http://localhost:5174");
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://foodnest-frontend.vercel.app");
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
